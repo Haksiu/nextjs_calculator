aa1430088c91680cb68e61d0a58f673b705ec35a
Haksiu <38468996+Haksiu@users.noreply.github.com>
2022-09-08 12:53:54 +0200

    calculator

--- pages/index.js
+++ pages/index.js
@@ -50,9 +50,9 @@ export default function Home() {
         <meta name="description" content="Nextjs Calculator" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <div className='flex min-h-[100vh] items-center justify-center p-[16px]'>
-      <div className='w-[100%] max-w-[400px] bg-white rounded-[16px] overflow-hidden'>
-        <div className='p-[16px] text-right bg-gray-900 text-gray-300 text-[24px] font-[500]'>
+    <div className='flex min-h-[100vh] items-center justify-center p-[18px]'>
+      <div className='w-[100%] max-w-[400px] bg-white rounded-[14px] overflow-hidden'>
+        <div className='p-[24px] text-right bg-gray-900 text-gray-300 text-[24px] font-[500]'>
           { result ? <span className='text-[16px] px-1 text-gray-500 min-w-[324px]'>({ result })</span> : '' } {calc || "0"}
         </div>
 
@@ -73,7 +73,9 @@ export default function Home() {
           { digitsButtons() }
           <button onClick={() => updateCalculator('0')} className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-[0.4s] hover:opacity-[0.9] flex-2 max-w-[33.333%] bg-gray-900'>0</button>
           <button onClick={() => updateCalculator('.')} className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-[0.4s] hover:opacity-[0.9] flex-2 max-w-[33.333%] bg-gray-900'>.</button>
-          <button onClick={() => calculate()} className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-[0.4s] hover:opacity-[0.9] flex-2 max-w-[33.333%] bg-gray-900'>=</button>
+          <button onClick={() => calculate()} className='appearance-none border-0 outline-0 text-gray-300 text-[20px] p-[16px] cursor-pointer transition-[0.4s] hover:opacity-[0.9] flex-2 max-w-[33.333%] bg-gray-900'>
+            =
+            </button>
         </div>
       </div>
     </div>
